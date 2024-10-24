91467c61165afba4ea208816185e3ef56dfd2510
AxoltDash <darshan@ciencias.unam.mx>
2024-10-23 20:43:21 -0600

    🔥 global.gd removing unused code

--- assets/scripts/global.gd
+++ assets/scripts/global.gd
@@ -9,4 +9,3 @@ func get_axis() -> Vector2:
 	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	axis.y = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
 	return axis.normalized()
-d
