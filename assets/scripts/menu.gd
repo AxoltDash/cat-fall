a4b46863f0a6ff801197110c27892db481f808cb
AxoltDash <darshan@ciencias.unam.mx>
2024-10-25 17:01:02 -0600

    :tada: Menu Finished

--- assets/scripts/menu.gd
+++ assets/scripts/menu.gd
@@ -7,7 +7,7 @@ func _ready():
 	_animated_sprite.play("rotationPlanet")
 	
 func _on_exit_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	get_tree().quit()
 
 func _on_start_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
