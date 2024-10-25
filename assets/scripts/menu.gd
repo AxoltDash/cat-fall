9b568fe16667a42295a415297fd369bc6ebc5d98
AxoltDash <darshan@ciencias.unam.mx>
2024-10-25 16:56:20 -0600

    💄 Added Planet Rotation Menu

---
+++ assets/scripts/menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var _animated_sprite = $PlanetSprite
+
+# func to start the animation of the menu when start the game
+func _ready():
+	_animated_sprite.play("rotationPlanet")
+	
+func _on_exit_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+func _on_start_pressed() -> void:
+	get_tree().quit()
