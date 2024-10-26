0a73c522b55febc7d94edbed62770c2f1601c257
AxoltDash <darshan@ciencias.unam.mx>
2024-10-25 20:18:02 -0600

    ✨ Finish Background Level

--- assets/scripts/level.gd
+++ assets/scripts/level.gd
@@ -12,13 +12,15 @@ func _process(delta) -> void:
 	$PathSpawn/PathFollow2D.set_progress($PathSpawn/PathFollow2D.get_progress() + 80 * delta)
 
 func parallax_bg(delta_time) -> void:
-	get_node("Background/Back").scroll_base_offset -= Vector2(1, 0) * delta_time * 8
-	$Bacground/Stars.scroll_base_offset -= Vector2(1, 0) * delta_time * 16
-	$Bacground/Planet_big.scroll_base_offset -= Vector2(1, 0) * delta_time * 24
-	$Bacground/Planet_small.scroll_base_offset -= Vector2(1, 0) * delta_time * 32
-	$Bacground/Planet_mini.scroll_base_offset -= Vector2(1, 0) * delta_time * 40
+	get_node("Background/Back").scroll_base_offset -= Vector2(32, 0) * delta_time * 0.60
+	$Background/Stars.scroll_base_offset -= Vector2(32, 0) * delta_time * 0.75
+	$Background/Planet_big.scroll_base_offset -= Vector2(1, 0) * delta_time * 40
+	$Background/Planet_small.scroll_base_offset -= Vector2(1, 0) * delta_time * 30
+	$Background/Planet_mini.scroll_base_offset -= Vector2(1, 0) * delta_time * 24
 
 func _on_timer_timeout() -> void:
-	var enemy_instance = enemy.instance()
-	enemy_instance.global_position = $PathSpawn/PathFollow2D.global_position
-	add_child(enemy_instance)
+	pass
+	# var enemy_instance = enemy.instance()
+	# enemy_instance.global_position = $PathSpawn/PathFollow2D.global_position
+	# add_child(enemy_instance)
+
