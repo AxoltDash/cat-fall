26dba4a3cdf6bb6d819df454650492726b7b8b39
AxoltDash <darshan@ciencias.unam.mx>
2024-10-28 21:57:36 -0600

    🚚 Changed GUI to GAMEOVER

---
+++ assets/scripts/gameover.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var _animation_player = $AnimationPlayer
+@onready var _animated_sprite = $PlanetSprite
+@onready var _audio_player = $XDDDmusic
+
+func _ready():
+	_animation_player.play("fade_out")
+	_animated_sprite.play("rotationPlanet")
+	_audio_player.play()
+	
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_restart_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
