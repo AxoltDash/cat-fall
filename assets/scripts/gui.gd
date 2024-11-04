21f7326ca326155ca63c448270b87f02e7ae4e6d
AxoltDash <darshan@ciencias.unam.mx>
2024-11-04 14:49:10 -0600

    ✨ Update GUI to display lives instead of credits

--- assets/scripts/gui.gd
+++ assets/scripts/gui.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
 
 func _process(_delta):
-	%Credits.text = "CREDITS: " + str(GLOBAL.credits)
+	%Credits.text = "LIVES: " + str(GLOBAL.credits)
 	%Score.text = "SCORE: " + str(GLOBAL.score)
